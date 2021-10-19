01f407008a96ef6748bddf1bb448932efc24db74
josephkem <josephkemoy@gmail.com>
2021-10-19 18:57:44 -0400

    Added specs for the map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { CssBaseline } from "@material-ui/core";
 import Header from "./components/Header";
+import Map from "./components/Map";
 
 function App() {
   return (
     <div>
       <CssBaseline />
       <Header />
+      <Map />
     </div>
   );
 }
---
+++ src/components/Map.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { makeStyles, Box } from "@material-ui/core";
+import GoogleMapReact from "google-map-react";
+
+const useStyles = makeStyles((theme) => ({
+  mapContainer: {
+    height: "85vh",
+    width: "100%",
+  },
+}));
+
+const Map = () => {
+  const classes = useStyles();
+  return (
+    <Box className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.GOOGLE_MAP_API_KEY }}
+        defaultCenter={{ lat: 32.6074954, lng: 43.941492 }}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={{ disableDefaultUI: true, zoomControl: true }}
+      />
+    </Box>
+  );
+};
+
+export default Map;
