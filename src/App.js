4ca8a0419cb5355e05ebf586c7fa62da8bdd61a5
josephkem <josephkemoy@gmail.com>
2021-10-23 18:52:19 -0400

    Created variables to set the type of locations on the map and created hooks for them in the main app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import { CssBaseline, Grid } from "@material-ui/core";
+import { useState } from "react";
 import Header from "./components/Header";
 import Map from "./components/Map";
 import List from "./components/List";
 
 function App() {
+  const [type, setType] = useState("restaurant");
+  const [loading, setLoading] = useState(false);
+  const [places, setPlaces] = useState([]);
+  const [childClicked, setChildClicked] = useState(null);
   return (
     <div>
       <CssBaseline />
       <Header />
       <Grid container style={{ width: "100%" }}>
         <Grid xs={12} md={4}>
-          <List />
+          <List
+            type={type}
+            setType={(type) => setType(type)}
+            childClicked={childClicked}
+          />
         </Grid>
         <Grid xs={12} md={8}>
           <Map />
--- src/components/List.js
+++ src/components/List.js
@@ -1,17 +1,39 @@
-import { makeStyles } from "@material-ui/core";
+import {
+  FormControl,
+  InputLabel,
+  makeStyles,
+  MenuItem,
+  Select,
+} from "@material-ui/core";
 import React from "react";
 
 const useStyles = makeStyles((theme) => ({
   container: {
     padding: 25,
   },
+  formControl: {
+    margin: 10,
+    minWidth: 120,
+    marginBottom: 30,
+  },
 }));
 
-const List = () => {
+const List = ({ type, setType, isLoading, childClicked, places }) => {
   const classes = useStyles();
   return (
     <div className={classes.container}>
-      <h1>List</h1>
+      <FormControl className={classes.formControl}>
+        <InputLabel id="type">Type: </InputLabel>
+        <Select
+          id="placeType"
+          value={type}
+          onChange={(e) => setType(e.target.value)}
+        >
+          <MenuItem value="restaurants">Restaurants</MenuItem>
+          <MenuItem value="hotels">Hotels</MenuItem>
+          <MenuItem value="attractions">Attractions</MenuItem>
+        </Select>
+      </FormControl>
     </div>
   );
 };
