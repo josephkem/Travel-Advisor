5b02330162ed4a6ca4401f31a22e754a985e304a
josephkem <josephkemoy@gmail.com>
2023-02-06 22:05:11 -0500

    Changed center variable to show map

--- src/components/Map.js
+++ src/components/Map.js
@@ -30,7 +30,7 @@ const Map = ({ places, coords, setBounds, setCoords, setChildClicked }) => {
     <Box className={classes.mapContainer}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
-        defaultCenter={coords}
+        center={coords}
         defaultZoom={14}
         margin={[50, 50, 50, 50]}
         options={{ disableDefaultUI: true, zoomControl: true }}
